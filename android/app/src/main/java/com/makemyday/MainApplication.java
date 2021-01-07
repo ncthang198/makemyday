2db712dd37893ab142e41aa31815a2fdeb765a42
Thang Nguyen <ncthang198@gmail.com>
2021-01-07 14:08:39 +0700

    get ramdom quote and push daily motivation

--- android/app/src/main/java/com/makemyday/MainApplication.java
+++ android/app/src/main/java/com/makemyday/MainApplication.java
@@ -4,12 +4,15 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
