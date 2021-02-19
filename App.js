402db4087f1584985c1c0828f5c4f3f507c0ef17
Thang Nguyen <ncthang198@gmail.com>
2021-02-19 15:29:47 +0700

    hoàn thành cơ bản

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, RefreshControl, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { StatusBar, RefreshControl, SafeAreaView, ScrollView, StyleSheet, Text, ToastAndroid } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 const wait = (timeout) => {
   return new Promise(resolve => setTimeout(resolve, timeout));
@@ -16,8 +18,30 @@ const App = () => {
 
   useEffect(() => {
     getQuoteRandom();
+    getFcmToken();
+    createListenerMessage();
   }, [])
 
+  getFcmToken = async () => {
+    let fcmToken = await messaging().getToken();
+    console.log("fcmToken: ", fcmToken)
+  }
+  createListenerMessage = async () => {
+    //Foreground
+    messaging().onMessage((mess) => {
+      console.log(mess)
+      // pushNoti(mess)
+    });
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+      // pushNoti(remoteMessage)
+    });
+    const messageInCloseApp = await messaging().getInitialNotification();
+    if (messageInCloseApp) {
+      console.log("Open notification in quit app state")
+      Alert.alert("Quit App")
+    }
+  }
   getQuoteRandom = () => {
     fetch("https://type.fit/api/quotes")
       .then(function (response) {
@@ -30,6 +54,9 @@ const App = () => {
         setQuote(data[randomNumber])
       });
   }
+  const showToast = () => {
+    ToastAndroid.show("Copy to clipboard !", ToastAndroid.SHORT);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar
@@ -45,8 +72,8 @@ const App = () => {
           />
         }
       >
-        {quote && <Text style={{ textAlign: "center" }}>{quote.text}</Text>}
-        {quote && <Text style={{ textAlign: "center", fontWeight: "bold", marginTop: 10 }}>"{quote.author?quote.author:"anonymous"}"</Text>}
+        {quote && <Text onPress={() => { Clipboard.setString(quote.text); showToast() }} style={{ textAlign: "center" }}>{quote.text}</Text>}
+        {quote && <Text onPress={() => { Clipboard.setString(quote.author); showToast() }} style={{ textAlign: "center", fontWeight: "bold", marginTop: 10 }}>"{quote.author ? quote.author : "anonymous"}"</Text>}
       </ScrollView>
     </SafeAreaView>
   );
